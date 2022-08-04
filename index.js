f869e9bdda9ac6cd15de1930ac2b936233206011
elaf swara <elafswara4@gmail.com>
2022-08-04 21:44:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,64 @@
+function add(){
+    return (2+3);
+}
+add();
+
+function subtract(){
+    return (5-2);
+}
+subtract();
+
+function multiply(){
+    return (5*2);
+}
+multiply();
+
+function divide(){
+    return (5/2);
+}
+divide();
+
+function add(a,b){
+    return (a+b);
+}
+add(2,3);
+
+function subtract(a,b){
+    return (a-b);
+}
+subtract(3,5);
+
+function multiply(a,b){
+    return (a*b);
+}
+multiply(3,2);
+
+function divide(a,b){
+    return (a/b);
+}
+divide(6,12);
+
+
+function increment(n){
+    n++;
+   return (n);
+}
+increment(3);
+
+function decrement(n){
+    n--;
+   return (n);
+}
+decrement(3);
+
+function makeInt(n){
+
+ return(parseInt(n, 10));
+}
+makeInt("2");
+
+function preserveDecimal(n){
+
+    return(parseFloat(n));
+}
+makeInt(2.33);
